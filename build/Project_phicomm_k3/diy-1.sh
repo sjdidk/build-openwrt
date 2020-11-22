4308634c6bcafaab9cf729a073c66e9e79adaa86
sjdidk <72053910+sjdidk@users.noreply.github.com>
2020-11-22 12:46:41 +0800

    Update diy-1.sh

--- build/Project_phicomm_k3/diy-1.sh
+++ build/Project_phicomm_k3/diy-1.sh
@@ -3,7 +3,9 @@
 #
 # 说明：
 # 除了第一行的#!/bin/bash不要动，其他的设置，前面带#表示不起作用，不带的表示起作用了（根据你自己需要打开或者关闭）
-
+git clone https://github.com/destan19/OpenAppFilter.git package/lean/OpenAppFilter
+git clone https://github.com/ozon/luci-app-ttyd.git package/feeds/luci-app-ttyd
+git clone https://github.com/zxl78585/luci-app-filetransfer.git package/luci-app-filetransfer
 
 #源码里面几乎囊括了所有插件，所以基本不需要添加插件了
 
